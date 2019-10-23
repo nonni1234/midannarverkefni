da105d193a3abf72f3808754c8a4e4c232a01722
unknown <nonni1234567@gmail.com>
2019-10-23 16:17:44 +0000

    breytti run skipuninni

--- app.py
+++ app.py
@@ -97,4 +97,5 @@ def pagenotfound(error):
     return template("404.html"), 404
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True)
+    #app.run(debug=True, use_reloader=True)
+    app.run()
